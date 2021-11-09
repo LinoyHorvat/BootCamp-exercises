f53f94dc896c1115de048115511ffe0192e3fcbd
LinoyHorvat <linoy.horvat@gmail.com>
2021-11-09 14:06:06 +0200

    li

---
+++ JavaScript/week3/5.3-password_validation.js
@@ -0,0 +1,14 @@
+function validFunc (password){
+  // 2
+  // password.length > 7 ? console.log ("Strong") : console.log ("Weak");
+// 3
+  console.log("Strong" || "Weak");
+
+// 4
+  // password.length > 7 && password != password.toLowerCase() ? console.log("Very Strong") : password.length > 7 ? console.log ("Strong") : console.log ("Weak");
+  
+}
+
+console.log(validFunc ("12jhkjh88"));
+console.log(validFunc ("12jhkjh88"));
+console.log(validFunc ("12jhkjh88"));
---
+++ JavaScript/week3/5.4-dark_or_light_color.js
@@ -0,0 +1,16 @@
+function whichColor(color){
+  switch (color.toLowerCase()){
+    case "yellow":
+    case "pink":
+    case "orange":
+      console.log("light color")
+      break;
+    case "blue":
+    case "purple":
+    case "brown":
+      console.log("dark color")
+      break;
+    default:
+      console.log("Unknown color")
+  }
+}
