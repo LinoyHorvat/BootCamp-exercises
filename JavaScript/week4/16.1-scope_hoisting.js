8117b2e53281dc9527c36d8d82e5fa3af3d11025
LinoyHorvat <linoy.horvat@gmail.com>
2021-11-18 09:47:35 +0200

    l

--- JavaScript/week4/16.1-scope_hoisting.js
+++ JavaScript/week4/16.1-scope_hoisting.js
@@ -1,7 +1,7 @@
-//1
+// Block 1 //////////////////////////////////////////////////////////////////
 
 // undefined
-//2
+// 2
 
 function funcA() {
   let a = 1;
@@ -13,16 +13,77 @@ function funcA() {
 }
 // funcA();
 
-// 2
+// Block 2 //////////////////////////////////////////////////////////////////
+
+// Aurelio De Rosa
+//John Doe or undefined depending where the code run.
 var fullName = "John Doe";
 var obj = {
   fullName: "Colin Ihrig",
   prop: {
     fullName: "Aurelio De Rosa",
     getFullName: function () {
+      console.log(this);
       return this.fullName;
     },
   },
 };
-console.log(obj.prop.getFullName());
-obj.prop.getFullName;
+// console.log(obj.prop.getFullName());
+// var test = obj.prop.getFullName;
+// console.log(test());
+
+// Block 3 //////////////////////////////////////////////////////////////////
+// undefined
+// number
+// Declaration of a global variable b.
+
+function funcB() {
+  a = b = 0;
+  a++;
+  return a;
+}
+
+funcB();
+// console.log(typeof a);
+// console.log(typeof b);
+
+// Block 4 //////////////////////////////////////////////////////////////////
+//2
+//2
+//the function
+function funcC() {
+  console.log("1");
+}
+// funcC();
+function funcC() {
+  console.log("2");
+}
+// funcC();
+
+// Block 5 //////////////////////////////////////////////////////////////////
+// -
+// 1
+//-
+// error - because "var" inside a function behaves as "let".
+
+function funcD1() {
+  d = 1;
+}
+funcD1();
+// console.log(d);
+
+function funcD2() {
+  var e = 1;
+}
+funcD2();
+// console.log(e);
+
+// Block 6 //////////////////////////////////////////////////////////////////
+// undefined
+// 1
+function funcE() {
+  console.log("Value of f in local scope: ", f);
+}
+console.log("Value of f in global scope: ", f);
+var f = 1;
+funcE();
