e2be9c3ddd691e523bcf09ac870088cb51d9124b
LinoyHorvat <linoy.horvat@gmail.com>
2021-11-22 17:29:41 +0200

    js

--- JavaScript/week5-advanced-ex/DataStructureImplementation.js
+++ JavaScript/week5-advanced-ex/DataStructureImplementation.js
@@ -1,7 +1,6 @@
-// Data structure implementation
-//  Array, Queue, Stack, Linked List
+// Data structure implementation of Array, Queue, Stack, Linked List
 
-// push(x) 
+// push(x)
 // pop(x)
 // contains(x) - is an element inside the data structure
 // addToStart(x)
@@ -13,46 +12,77 @@
 // Stack
 class Stack {
   constructor() {
-    this.items = [] ;
-  } 
+    this.items = [];
+  }
   push(value) {
-    this.items[this.items.length] = value
+    this.items[this.items.length] = value;
   }
-  pop(){
-    let value = this.items[this.items.length] ;
-    this.items = this.items.splice(-1)
-    return value
+  isEmpty() {
+    this.items.length === 0 ? true : false;
   }
-  isEmpty(){
-    this.items.length === 0 ? true : false ; 
+  pop() {
+    if (!this.isEmpty) {
+      let value = this.items[this.items.length];
+      this.items = this.items.splice(-1);
+      return value;
+    } else return "Can't pop because stack is empty";
   }
 }
-const stack1 = new Stack(); 
+const stack1 = new Stack();
 
 // Queue
 class Queue {
   constructor() {
-    this.items = [] ;
-  } 
-  isEmpty(){
-    this.items.length === 0 ? true : false ; 
+    this.items = [];
+  }
+  isEmpty() {
+    this.items.length === 0 ? true : false;
   }
   push(value) {
-    this.items[this.items.length] = value
+    this.items[this.items.length] = value;
   }
-  pop(){
-    let value = this.items[0] ;
-    this.items = this.items.splice(1)
-    return value
+  pop() {
+    if (!this.isEmpty) {
+      let value = this.items[0];
+      this.items = this.items.splice(1);
+      return value;
+    } else return "Can't pop because queue is empty";
+  }
+  isEmpty() {
+    this.items.length === 0 ? true : false;
   }
 }
-const queue1 = new Queue(); 
+const queue1 = new Queue();
 
 //Array
+class Array {
+  constructor() {
+    this.items = {};
+    this.length = 0;
+  }
+  push(val) {
+    this.items[this.length] = val;
+    this.length++;
+  }
+  pop() {
+    if (!this.isEmpty) {
+      let value = this.items[this.length - 1];
+      delete this.items[Object.keys(this.items)[this.length - 1]];
+      array1.length--;
+      return value;
+    } else return "Can't pop because array is empty";
+  }
+  contains(x){}
+  addToStart(x){}
+  addToEnd(x){}
+  isFull(){}
+  removeFirst(x){}
+}
+const array1 = new Array();
+array1.push(2);
+array1.push(3);
+array1.push(4);
+console.log(array1);
+// console.log(array1.pop());
 
-
-
-
-
-
-
+// Linked List
