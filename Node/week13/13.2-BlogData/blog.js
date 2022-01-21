9c57025f70de11858c1cbfa2283d4e89fe2ec8b9
LinoyHorvat <linoy.horvat@gmail.com>
2022-01-21 16:53:50 +0200

    week13

--- Node/week13/13.2-BlogData/blog.js
+++ Node/week13/13.2-BlogData/blog.js
@@ -1,23 +1,3 @@
-
-// const mongodb = require('mongodb');
-// const MongoClient = mongodb.MongoClient;
-
-// const connectionURL =  'nomongodb://127.0.0.1:27017'
-// const databaseName = 'blog-data'
-
-// MongoClient.connect(connectionURL,{useNewUrlParser: true}, (error, client) => {
-//   if(error) return console.log('Unable to connect to database');
-//   console.log('Connected correctly!');
-
-
-//   const db = client.db(databaseName);
-//   db.collection('users').insertOne({
-//     name: 'Linoy',
-//     email: 'linoy@gmail.com'
-//   }
-//   )
-// });
-
 /*
 users,posts,comments
 users has own collection
---
+++ Node/week13/products.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+mongoose.connect("mongodb://127.0.0.1:27017/products", {});
+
+const Product = mongoose.model("Product", {
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  details: {
+    description: {
+      type: String,
+      required: true,
+      minLength: 10,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    discount: {
+      type: Number,
+      default: 0,
+    },
+    images: {
+      type: [String],
+      minItems: 2,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isMobilePhone(value, "he-IL")) {
+          throw new Error("Not a valid Isreali phone number");
+        }
+      },
+    },
+    dataAdded: {
+      type: Date,
+      default: Date.now(),
+    },
+  },
+});
+
+const newProduct = new Product({
+  name: "xbox",
+  category: "console",
+  details: {
+    description: "gaming-console",
+    price: 200,
+    discount: 0,
+    phoneNumber: "0505234560",
+  },
+});
+
+newProduct.save();
